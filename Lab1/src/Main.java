d2c10a5a91d657befa6afbb915e298b60085c2f2
ArtemGomer <43440717+ArtemGomer@users.noreply.github.com>
2020-02-22 22:02:58 +0700

    added LogManager

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,13 +1,21 @@
 import calculator.Calculator;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main {
     private final static Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
+        LogManager logManager = LogManager.getLogManager();
+        try {
+            logManager.readConfiguration(new FileInputStream("logConfig.properties"));
+        } catch (IOException ex){
+            logger.log(Level.WARNING, "Can not read properties for logging");
+        }
         String inFileName = null;
         if (args.length > 1) {
             logger.log(Level.SEVERE,"Too much arguments for programme {0}. End of work.", Arrays.toString(args));
