1f1031364ec2c93a2ea59469e00d12f08a0a67f0
ArtemGomer <43440717+ArtemGomer@users.noreply.github.com>
2020-03-08 12:21:52 +0700

    Gradle + factoryExceptions

--- Lab1/Tests/RunTests.java
+++
@@ -1,9 +0,0 @@
-import calculator.operations.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@Suite.SuiteClasses( {AdditionTest.class, ContextTest.class, DefineTest.class, DivisionTest.class, MultiplicationTest.class,
-PopTest.class, PrintTest.class, PushTest.class, SqrtTest.class, SubtractionTest.class})
-@RunWith(Suite.class)
-public class RunTests {
-}
